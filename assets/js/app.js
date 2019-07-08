5cbc9c50c6ae7333e3e30304036229e404e17fa9
tensor-programming <abeltensor@tensor-programming.com>
2019-07-08 17:37:21 -0400

    add some details

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,7 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
